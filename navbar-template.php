ed81898147cbf0d18ee562161837304c7fe1a4f7
ObiDev <marcinobi@gmail.com>
2016-10-27 11:51:51 +0200

    Add files via upload

---
+++ navbar-template.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container-flex" id="menu">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-menu">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" id="logo" href="<?php bloginfo('url'); ?>"></a>
+		</div>
+		<!-- <?php
+			wp_nav_menu( array(
+				"theme-location"=>"primary",
+				"container"=>"div",
+				"container_class"=>"collapse navbar-collapse",
+				"container_id"=>"main-menu",
+				"menu_class"=>"nav navbar-nav navbar-right",
+				"items_wrap"=>'<ul class=" %2$s "> %3$s </ul>'
+			));
+		?> -->
+
+		<ul id="menu-list">
+			<li><a href="x">dfd</a></li>
+			<li><a href="x">dew</a></li>
+			<li><a href="x">ss</a></li>
+			<li><a href="x">fd</a></li>
+			<li><a href="x">d</a></li>
+			<li><a href="x">fds</a></li>
+		</ul> 
+	</div>
+</nav>
